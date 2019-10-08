027e0a9bec79a08e929161ac857b67c76a04318a
Vitaly Isaev <vitaly.isaev@ncloudtech.ru>
2019-10-08 15:10:12 +0300

    Failed to compile

--- lib_test.go
+++ lib_test.go
@@ -2,13 +2,15 @@ package cgomultiversion
 
 import (
 	"testing"
+	cgomultiversion1 "github.com/vitalyisaev2/cgo_multiversion_go_lib/v1"
+	cgomultiversion2 "github.com/vitalyisaev2/cgo_multiversion_go_lib/v2"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAdd(t *testing.T) {
 	// в первой версии библиотеки обычное сложение
-	assert.Equal(t, 10, AddV1(5, 5))
+	assert.Equal(t, 10, cgomultiversion1.Add(5, 5))
 	// во второй версии библиотеки под сложениим скрывается умножение
-	assert.Equal(t, 25, AddV2(5, 5))
+	assert.Equal(t, 25, cgomultiversion2.Add(5, 5))
 }
--- v1/lib.go
+++ v1/lib.go
@@ -4,6 +4,6 @@ package cgomultiversion
 // #include <cgomultiversion_v1/lib.h>
 import "C"
 
-func AddV1(a, b int) int {
+func Add(a, b int) int {
 	return int(C.add(C.int(a), C.int(b)))
 }
